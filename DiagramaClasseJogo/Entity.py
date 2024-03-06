ed48c1b99397a422097199eda8be0aa82359227e
Patricky Lucas <patrickylucas@hotmail.com>
2024-03-06 02:48:12 -0300

    Signed-off-by: Patricky Lucas <patrickylucas@hotmail.com>

---
+++ DiagramaClasseJogo/Entity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Entity:
+    def __init__(self):
+        self.Nome = None
+        self.screen = None
+        self.rect = None
+
+    def Move(self, ):
+        pass
+
---
+++ DiagramaClasseJogo/Level.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Level:
+    def __init__(self):
+        self.screen = None
+        self.entityList = None
+        self.name = None
+
+    def Run(self, ):
+        pass
+
